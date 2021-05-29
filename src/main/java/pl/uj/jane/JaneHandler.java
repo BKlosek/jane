25871f4559f898d9e8851cdd6a41593d4f117f35
bartosz.klosek <bartosz.klosek@ocado.com>
2021-05-29 14:41:07 +0200

    Initial commit

---
+++ src/main/java/pl/uj/jane/JaneHandler.java
@@ -0,0 +1,36 @@
+package pl.uj.jane;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.builder.StandardSkillBuilder;
+import com.amazon.ask.model.RequestEnvelope;
+import com.amazon.ask.model.ResponseEnvelope;
+import com.amazon.ask.model.services.util.JacksonSerializer;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.util.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class JaneHandler implements RequestStreamHandler {
+
+    private final Skill skill;
+    private final JacksonSerializer jacksonSerializer;
+
+    public JaneHandler() {
+        this.skill = new StandardSkillBuilder().addRequestHandler(new RandomNumberHandler()).build();
+        this.jacksonSerializer = new JacksonSerializer();
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        context.getLogger().log("Request recieved");
+        String request = IOUtils.toString(inputStream);
+        RequestEnvelope requestEnvelope = new JacksonSerializer().deserialize(request, RequestEnvelope.class);
+        ResponseEnvelope responseEnvelope = this.skill.invoke(requestEnvelope);
+        byte[] response = jacksonSerializer.serialize(responseEnvelope).getBytes(StandardCharsets.UTF_8);
+        outputStream.write(response);
+    }
+}
---
+++ src/main/java/pl/uj/jane/RandomNumberHandler.java
@@ -0,0 +1,19 @@
+package pl.uj.jane;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+public class RandomNumberHandler implements RequestHandler {
+    @Override
+    public boolean canHandle(HandlerInput handlerInput) {
+        return true;
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput handlerInput) {
+        return handlerInput.getResponseBuilder().withSpeech("BAJO JAJO").build();
+    }
+}
